19d63ec6edd54109f48665075c8974a336b16af2
Hubert <129938602+Fr-Hubert@users.noreply.github.com>
2024-09-12 18:15:36 +0900

    関数

--- Lua/Chapter4.lua
+++ Lua/Chapter4.lua
@@ -1,7 +1,6 @@
---関数
+--[[【関数】]]--
 
-410
---関数の作り方
+--[[関数の作り方]]--
 
 --では関数を作ってみよう！
 --関数を作るには下記のようにする。
@@ -40,7 +39,7 @@ print(value1, value2)
 --としても，x,yはvalue1, value2のコピーなので，中身をいくら書き換えても呼び出しもとのvalue1, value2には影響を及ばさない。
 --引数に関して、もしかしたら何も受け取る必要がない場合があるかもしれない。そういった場合は、引数の中は空にしておく。
 
---関数の戻り値
+--[[関数の戻り値]]--
 
 --関数には戻り値と呼ばれるものが存在している。
 --上記とその前のコードを見たらreturnは値を関数を呼び出した側に返す命令であって、x+yの計算結果を呼び出し元に返している。
@@ -91,7 +90,7 @@ print(value1, value2)
 --この場合，value2にはnilが代入される。value2は20ではないのでご注意！！
 
 
---何も返さない関数
+--[[何も返さない関数]]--
 
 --戻り値の関数，つまり何も返さない関数を作ることもできる。その場合returnを書かなければ良い
 
@@ -115,5 +114,205 @@ func()
 --上記の二つのコードは全く同じものである。
 
 
+--[[変数に関数を代入する]]--
 
+--Luaの変数にはどんな値でも代入することができる。
+--例え関数であろうとも代入が可能！！
+--例えば次のようなことも可能である。
+
+local function sum(x, y)
+  return x + y
+end
+
+local function mul(x, y)
+  return x * y
+end
+
+local value = sum
+print("value is " .. type(value));
+print("11 + 13 is " .. value(11, 13))
+
+local value = mul
+print("11 * 13 is " .. value(11, 13))
+
+--実行結果
+value is function
+11 + 13 is 24
+11 * 13 is 143
+
+--type関数は現在の変数の型を調べるもので、valueにはsum関数やmul関数を代入している。
+--そして、valueを使い、関数にアクセスすることもできる。
+--C言語の関数ポインタのようなことができると思ってもよい。
+
+
+--[[関数中に関数を定義する]]--
+
+--Luaでは関数内に関数を定義することも可能である。
+
+local function func(x)
+    local function get()
+      return x
+    end
+
+    local function add(value)
+      x = x + value
+    end
+    return get, add
+end
+
+firstGetValue, firstAddValue = func(10)
+secondGetValue,secondAddValue = func(30)
+
+print("firstvalue : " ..firstGetValue())
+print("secondvalue : " .. secondGetValue())
+
+firstAddValue(15)
+secondAddValue(20)
+
+print("firstvalue : " .. firstGetValue())
+print("secondvalue :" .. secondGetValue())
+    
+--func関数はgetとadd関数を戻り値として返している。
+
+--実行結果
+--firstvalue: 10
+--secondvalue: 30
+--firstvalue: 25
+--secondvalue: 50
+
+
+--[[無名関数]]--
+
+--名前無し関数を作成することも可能である。
+--例えば次のようなコードを書くことができる。
+
+local function createSquare()
+  return function(x)
+    return x * x
+  end
+end
+  
+square = createSquare()
+print("10 * 10 is " .. square(10, 10) )
+
+--createSquare関数は戻り値にx*xを行う関数を返している。
+
+―‐実行結果
+―‐10*10is100
+
+--[[標準ライブラリ]]--
+
+--Luaには標準関数と呼ばれるものが存在する。
+--これは，よく使われる処理があらかじめ関数として用意されている。
+--例えばprint関数やio.read関数などが標準関数にあたる。
+
+--[[基本ライブラリ]]--
+
+--[assert関数]--
+
+--定義
+assert (v[, message])
+--assert関数はvが偽条件ならばエラーを発生させる関数である。
+--messageにはエラーメッセージを渡し、エラーメッセージは省略可能である。
+
+value = true
+assert(value, "Error #1")
+value = false
+assert(value, "Error #2")
+
+--実行結果
+--lua51m.exe:test.lua:4:Error #2
+--stacktraceback:
+--[C]: infunction assert
+--test.lua:4:inmain chunk
+--[C]: ?
+ 
+--[dofile関数]--
+
+--定義
+dofile(filename)
+  
+--dofile関数はfilenameで指定したファイルを実行する関数である。
+--例えば以下のようにして使う
+
+--今回は例として二つのLuaファイル，test.luaとcall.luaにコードを記述する。
+-- call.lua
+function testFunc()
+  print("Hello")
+  return"World!"
+end
+
+-- test.lua
+dofile("call.lua")
+
+value = testFunc()
+print(value)
+  
+実行結果
+Hello
+World!
+--dofileでcall.luaを読み込む事により，test.lua側でtestFunc関数を利用することができる。
+
+  
+--[type関数]--
+
+--定義
+type(v)
+
+-- type関数は，データの型を返す関数である。
+
+value = nil
+print("value : " .. type(value))
+value = 10
+print("value : " .. type(value))
+value = true
+print("value : " .. type(value))
+value = "Hello"
+print("value : " .. type(value))
+
+--実行結果
+--value : nil
+--value : number
+--value : boolean
+--value : string
+
+--[tonumber関数]--
+  
+--定義
+tonumber(e[,base])
+--tonumber関数は引数eを数値に変換する関数である。
+--数値変換が可能な場合は数値を返し，そうでなければnilを返す。
+--また，baseを指定すると，eをbase進法の数値として変換する。
+--省略すると，10が自動的に指定される。
+
+value1 = "25"
+print("value1 : " .. value1 .. " type : " .. type(value1))
+value2 = tonumber(value1)
+print("value2 : " .. value2 .. " type : " .. type(value2))
+
+--実行結果
+--value1 : 25 type : string
+--value2 : 25 type : number
+
+  
+ --[tostring関数]--
+
+--定義
+tostring(e)
+--tostring関数はeを文字列に変換する関数である。
+
+value1 = 25
+print("value1 : " .. value1 .. " value1 : " .. type(value1))
+value2 = tostring(value1)
+print("value2 : " .. value2 .. " type : " .. type(value2))
+
+--実行結果
+--value1 : 25 type : number
+--value2 : 25 type : string
+
+--数値から文字列に変換する際は，わざわざtostring関数を使用しなくても，空の文字列を追加するだけでよい。
+  
+value2 = value2 .. ""
+
+  
 作成中...
